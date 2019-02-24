d98a45e60789fc3ba159151d243200e3d91e15da
Luis Raygadas <lraygada@pnw.edu>
2019-02-24 16:35:51 -0600

    Uploading java files

---
+++ FunWithSockets/CollectingServer.java
@@ -0,0 +1,10 @@
+/**This program creates a server that accepts names from client programs
+  *The server then accumulates the names and sends them back to the current client
+ */
+
+import java.io.*;
+
+public class CollectingServer
+{
+
+}
---
+++ FunWithSockets/NamedClient.java
@@ -0,0 +1,10 @@
+/**This program creates a client program that sends a name to a server program
+  *this program will then receive a list of names that the server has collected
+  */
+
+import java.io.*;
+
+public class NamedClient
+{
+
+}
